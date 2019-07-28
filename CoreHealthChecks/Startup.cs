80a372f3dd48a7c2455c8e868ae770607a227d5d
Amen <amen@hubtel.com>
2019-07-28 02:50:27 +0100

    Implement .Net Core inbuilt Health Check

--- CoreHealthChecks/Startup.cs
+++ CoreHealthChecks/Startup.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Threading.Tasks;
+using CoreHealthChecks.Services;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 
 namespace CoreHealthChecks
 {
@@ -25,6 +31,10 @@ namespace CoreHealthChecks
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHealthChecks()
+                .AddCheck("ping1", new PingHealthCheck("www.google.com", 3000))
+                .AddCheck("ping2", new PingHealthCheck("www.bing.com", 3000));
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -40,6 +50,22 @@ namespace CoreHealthChecks
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            var options = new HealthCheckOptions();
+            options.ResponseWriter = async (c, r) => {
+
+                c.Response.ContentType = "application/json";
+
+                var result = JsonConvert.SerializeObject(new
+                {
+                    status = r.Status.ToString(),
+                    errors = r.Entries.Select(e => new { key = e.Key, value = e.Value.Status.ToString() })
+                });
+
+                await c.Response.WriteAsync(result);
+            };
+
+            app.UseHealthChecks("/hc");
 
             app.UseHttpsRedirection();
             app.UseMvc();
